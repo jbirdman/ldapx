9be6ece4b81579150fcdd57e853b3a6f6de63d46
Jeffrey Bird <jeffrey.bird@jcu.edu.au>
2019-06-13 19:25:16 +1000

    Add mutable entry implementation.

--- entry.go
+++ entry.go
@@ -1,41 +1,174 @@
 package ldapx
 
 import (
-	"github.com/deckarep/golang-set"
+	"fmt"
 	"gopkg.in/ldap.v2"
 )
 
-func AddValue(attr string, values []string, value string) *ldap.PartialAttribute {
-	return AddValues(attr, values, []string{value})
+const (
+	CHANGE_ADD    = "add"
+	CHANGE_UPDATE = "update"
+	CHANGE_DELETE = "delete"
+)
+
+type Entry struct {
+	ChangeType string
+	DN         string
+	Attributes map[string]*ldap.EntryAttribute
+	Changes    []AttributeChange
 }
 
-func AddValues(attr string, values []string, addValues []string) *ldap.PartialAttribute {
-	currentValues := mapset.NewSetFromSlice(SliceToInterface(values))
-	newValues := mapset.NewSetFromSlice(SliceToInterface(addValues))
+type MutableEntry interface {
+	AddAttribute(attr string, value []string)
+	ReplaceAttribute(attr string, value []string)
+	DeleteAttribute(attr string, value []string)
+	Update(conn *Conn) error
+}
 
-	changes := newValues.Difference(currentValues)
+type AttributeChange struct {
+	Action string
+	Attr   string
+	Value  []string
+}
 
-	if changes.Cardinality() == 0 {
+func NewEntry(entry *ldap.Entry) Entry {
+	var dn string
+	attributes := make(map[string]*ldap.EntryAttribute)
+
+	var changeType = CHANGE_ADD
+	if entry != nil {
+		dn = entry.DN
+		changeType = CHANGE_UPDATE
+
+		// Copy in the attributes from the ldap entry
+		for _, a := range entry.Attributes {
+			attributes[a.Name] = a
+		}
+	}
+
+	return Entry{ChangeType: changeType, DN: dn, Attributes: attributes}
+}
+
+func (c *Entry) Print() {
+	fmt.Printf("DN: %s\n", c.DN)
+	for _, attr := range c.Attributes {
+		attr.Print()
+	}
+}
+
+func (c *Entry) Clear() {
+	c.ChangeType = CHANGE_ADD
+	c.Changes = nil
+}
+
+func (c *Entry) GetAttributeValues(attribute string) []string {
+	return c.Attributes[attribute].Values
+}
+
+func (e *Entry) GetAttributeValue(attribute string) string {
+	values := e.GetAttributeValues(attribute)
+	if len(values) == 0 {
+		return ""
+	}
+	return values[0]
+}
+
+func (c *Entry) AddAttributeChange(action string, attr string, value []string) {
+	c.Changes = append(c.Changes, AttributeChange{Action: action, Attr: attr, Value: value})
+}
+func (c *Entry) AddAttribute(attr string, value []string) {
+	a, ok := c.Attributes[attr]
+	if !ok {
+		a = ldap.NewEntryAttribute(attr, value)
+	} else {
+		a.Values = append(a.Values, value...)
+	}
+	c.Attributes[attr] = a
+	c.AddAttributeChange("add", attr, value)
+}
+
+func (c *Entry) ReplaceAttribute(attr string, value []string) {
+	c.Attributes[attr] = ldap.NewEntryAttribute(attr, value)
+	c.AddAttributeChange("replace", attr, value)
+}
+
+func (c *Entry) DeleteAttribute(attr string, value []string) {
+	old, ok := c.Attributes[attr]
+	if !ok {
+		return
+	}
+	if len(value) > 0 {
+		a := ldap.NewEntryAttribute(attr, nil)
+
+		for _, v := range old.Values {
+			found := false
+			for _, nv := range value {
+				if nv == v {
+					found = true
+					break
+				}
+			}
+
+			if !found {
+				a.Values = append(a.Values, v)
+			}
+		}
+		c.Attributes[attr] = a
+	} else {
+		delete(c.Attributes, attr)
+	}
+
+	c.AddAttributeChange("delete", attr, value)
+}
+
+func (c Entry) Update(conn *Conn) error {
+	if len(c.Changes) == 0 {
 		return nil
 	}
 
-	return &ldap.PartialAttribute{Type: attr, Vals: InterfaceToSlice(changes.ToSlice())}
-}
-
-func RemoveValue(attr string, values []string, value string) *ldap.PartialAttribute {
-	return RemoveValues(attr, values, []string{value})
-}
-
-func RemoveValues(attr string, values []string, removeValues []string) *ldap.PartialAttribute {
-	currentValues := mapset.NewSetFromSlice(SliceToInterface(values))
-	changes := mapset.NewSetFromSlice(SliceToInterface(removeValues))
-
-	removes := currentValues.Difference(changes)
-
-	if removes.Cardinality() == 0 {
-		return nil
+	switch c.ChangeType {
+	case CHANGE_ADD:
+		return conn.Add(buildAddRequest(c.DN, c.Changes))
+	case CHANGE_UPDATE:
+		return conn.Modify(buildModifyRequest(c.DN, c.Changes))
+	case CHANGE_DELETE:
+		return conn.Del(buildDelRequest(c.DN))
 	}
 
-	return &ldap.PartialAttribute{Type: attr, Vals: InterfaceToSlice(removes.ToSlice())}
+	return nil
 }
 
+func buildAddRequest(dn string, changes []AttributeChange) *ldap.AddRequest {
+	r := NewAddRequest(dn)
+
+	for _, change := range changes {
+		if change.Action != "add" && change.Action != "replace" {
+			continue
+		}
+
+		r.Attribute(change.Attr, change.Value)
+	}
+
+	return r
+}
+
+func buildModifyRequest(dn string, changes []AttributeChange) *ldap.ModifyRequest {
+	r := NewModifyRequest(dn)
+
+	for _, change := range changes {
+		switch change.Action {
+		case "add":
+			r.Add(change.Attr, change.Value)
+		case "replace":
+			r.Replace(change.Attr, change.Value)
+		case "delete":
+			r.Delete(change.Attr, change.Value)
+		}
+	}
+
+	return r
+}
+
+func buildDelRequest(dn string) *ldap.DelRequest {
+	return NewDelRequest(dn, nil)
+}
---
+++ entry_test.go
@@ -0,0 +1,43 @@
+package ldapx
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewEntry(t *testing.T) {
+	entry := NewEntry(nil)
+
+	entry.DN = "cn=test"
+	entry.AddAttribute("cn", []string{"test"})
+	entry.AddAttribute("objectclass", []string{"top", "person"})
+
+	entry.Print()
+
+	r := buildAddRequest(entry.DN,entry.Changes)
+	fmt.Printf("request: %+v\n",r)
+
+	assert.True(t, true)
+}
+
+func TestEntry_AddAttribute(t *testing.T) {
+	entry := NewEntry(nil)
+
+	entry.DN = "cn=test"
+	entry.AddAttribute("cn", []string{"test"})
+
+	assert.Equal(t,"test",entry.GetAttributeValue("cn"))
+}
+
+func TestEntry_ReplaceAttribute(t *testing.T) {
+	entry := NewEntry(nil)
+
+	entry.DN = "cn=test"
+	entry.AddAttribute("cn", []string{"test"})
+	entry.ReplaceAttribute("cn", []string{"test2"})
+
+	fmt.Println(entry.Changes)
+
+	assert.Equal(t,"test2",entry.GetAttributeValue("cn"))
+}
