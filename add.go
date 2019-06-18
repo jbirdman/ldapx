8fa0bb6929a93d29f86cf1fa54e1b455d3a61e44
Jeffrey Bird <jeffrey.bird@jcu.edu.au>
2019-06-18 14:16:34 +1000

    Re-implement idempotency on AttributeAddValues method to remove use of a set.

--- add.go
+++ add.go
@@ -1,31 +1,43 @@
 package ldapx
 
 import (
-	"github.com/deckarep/golang-set"
 	"gopkg.in/ldap.v2"
 )
 
 func (e *Entry) AddAttributeValues(attr string, value []string) {
 	var values []string
 
-	a, ok := e.Attributes[attr]
-	if !ok {
-		a = ldap.NewEntryAttribute(attr, value)
-		values = value
-	} else {
-		oldValues := mapset.NewSetFromSlice(SliceToInterface(a.Values))
-		newValues := mapset.NewSetFromSlice(SliceToInterface(value))
-
-		valueSet := newValues.Difference(oldValues)
-
-		for _, v := range valueSet.ToSlice() {
-			a.Values = append(a.Values, v.(string))
-			values = append(values, v.(string))
-		}
+	if len(value) == 0 {
+		return
 	}
 
-	if len(values) > 0 {
-		e.Attributes[attr] = a
+	a, ok := e.Attributes[attr]
+	if !ok {
+		e.Attributes[attr] = ldap.NewEntryAttribute(attr, value)
+		e.AddAttributeChange("add", attr, value)
+		return
+	}
+
+	var addedValues []string
+
+	for _, o := range value {
+		var found bool
+		for _, d := range a.Values {
+			if o == d {
+				found = true
+				break
+			}
+		}
+
+		if found {
+			continue
+		}
+
+		addedValues = append(addedValues, o)
+		a.Values = append(a.Values, o)
+	}
+
+	if len(addedValues) > 0 {
 		e.AddAttributeChange("add", attr, values)
 	}
 }
