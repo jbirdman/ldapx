d8f7e9235eafb6a4458f7373d7302e1409fd9fa9
Jeffrey Bird <jeffrey.bird@jcu.edu.au>
2023-01-30 22:58:48 +1000

    Commenting.

--- entry.go
+++ entry.go
@@ -11,18 +11,19 @@ import (
 )
 
 const (
-	ChangeAdd    = "add"
-	ChangeUpdate = "update"
-	ChangeDelete = "delete"
+	ChangeAdd    = "add"    // ChangeAdd is the change type for adding an entry
+	ChangeUpdate = "update" // ChangeUpdate is the change type for updating an entry
+	ChangeDelete = "delete" // ChangeDelete is the change type for deleting an entry
 )
 
+// Entry represents an LDAP entry
 type Entry struct {
-	DN                 string            `json:"dn"`
-	ChangeType         string            `json:"change_type"`
-	Attributes         AttributeMap      `json:"attributes,omitempty"`
-	Changes            []AttributeChange `json:"changes,omitempty"`
-	committed          bool
-	originalAttributes AttributeMap
+	DN                 string            `json:"dn"`                   // DN is the distinguished name of the entry
+	ChangeType         string            `json:"change_type"`          // ChangeType is the type of change to be applied to the entry
+	Attributes         AttributeMap      `json:"attributes,omitempty"` // Attributes is a map of attribute name to attribute
+	Changes            []AttributeChange `json:"changes,omitempty"`    // Changes is a list of changes to be applied to the entry
+	committed          bool              // committed is true if the entry has been committed to the server
+	originalAttributes AttributeMap      // originalAttributes is a copy of the attributes when the entry was created
 }
 
 var _ MutableEntry = &Entry{}
@@ -53,6 +54,7 @@ type AttributeChange struct {
 	Value  []string
 }
 
+// NewEntry creates a new entry with the given DN
 func NewEntry(dn string) *Entry {
 	return &Entry{
 		DN:                 dn,
@@ -62,6 +64,7 @@ func NewEntry(dn string) *Entry {
 	}
 }
 
+// NewEntryFromLdapEntry creates a new entry from an ldap entry
 func NewEntryFromLdapEntry(entry *ldap.Entry) *Entry {
 	e := NewEntry("")
 
@@ -80,55 +83,70 @@ func NewEntryFromLdapEntry(entry *ldap.Entry) *Entry {
 }
 
 func (e *Entry) ToLdapEntry() *ldap.Entry {
+	// Copy the attributes to a map
 	attrs := make(map[string][]string)
 
+	// Copy in the attributes from the ldap entry
 	for _, k := range e.AttributeNames() {
 		attrs[k] = e.GetAttributeValues(k)
 	}
 
+	// Create the ldap entry
 	return ldap.NewEntry(e.DN, attrs)
 }
 
 func (e *Entry) Print() {
+	// Print the entry
 	e.ToLdapEntry().Print()
 }
 
 func (e *Entry) PrettyPrint(indent int) {
+	// Print the entry
 	e.ToLdapEntry().PrettyPrint(indent)
 }
 
 func (e *Entry) AttributeNames() []string {
+	// Return the attribute names
 	return e.Attributes.AttributeNames()
 }
 
 func (e *Entry) ToLDIF() string {
+	// Create a string builder
 	var b strings.Builder
 	_, _ = fmt.Fprintf(&b, "dn: %s\n", e.DN)
 
+	// Sort the attributes
 	keys := e.AttributeNames()
 	sort.Strings(keys)
 
+	// Print the attributes
 	for _, k := range keys {
 		for _, v := range e.Attributes.Get(k).Values {
 			_, _ = fmt.Fprintf(&b, "%s: %s\n", k, v)
 		}
 	}
+
+	// Return the string
 	return b.String()
 }
 
 func (e *Entry) Changed() bool {
+	// Check if the entry has changed
 	return len(e.Changes) > 0
 }
 
 func (e *Entry) ResetChanges() {
+	// Reset the changes
 	e.Changes = nil
 }
 
 func (e *Entry) AttributeExists(attr string) bool {
+	// Check if the attribute exists
 	return e.Attributes.AttributeExists(attr)
 }
 
 func (e *Entry) GetAttributeValues(attribute string) []string {
+	// Get the attribute values
 	v := e.Attributes.Get(attribute)
 	if v == nil {
 		return nil
@@ -137,6 +155,7 @@ func (e *Entry) GetAttributeValues(attribute string) []string {
 }
 
 func (e *Entry) GetAttributeValue(attribute string) string {
+	// Get the attribute value
 	values := e.GetAttributeValues(attribute)
 	if len(values) == 0 {
 		return ""
@@ -145,14 +164,17 @@ func (e *Entry) GetAttributeValue(attribute string) string {
 }
 
 func (e *Entry) AddAttributeChange(action string, attr string, value []string) {
+	// Add the attribute change
 	e.Changes = append(e.Changes, AttributeChange{Action: action, Attr: attr, Value: value})
 }
 
 func (e *Entry) Update(conn *Conn) error {
+	// Update the entry
 	if !e.Changed() {
 		return nil
 	}
 
+	// Check if the entry has been committed
 	if e.committed {
 		return errors.New("entry can only be updated once")
 	}
@@ -160,10 +182,13 @@ func (e *Entry) Update(conn *Conn) error {
 
 	switch e.ChangeType {
 	case ChangeAdd:
+		// Add the entry
 		return conn.Add(buildAddRequest(e.DN, e.Changes))
 	case ChangeUpdate:
+		// Modify the entry
 		return conn.Modify(buildModifyRequest(e.DN, e.Changes))
 	case ChangeDelete:
+		// Delete the entry
 		return conn.Del(buildDelRequest(e.DN))
 	}
 
@@ -171,6 +196,7 @@ func (e *Entry) Update(conn *Conn) error {
 }
 
 func (e *Entry) Clone() *Entry {
+	// Clone the entry
 	dest := NewEntry(e.DN)
 
 	for _, a := range e.AttributeNames() {
@@ -181,13 +207,17 @@ func (e *Entry) Clone() *Entry {
 }
 
 func buildAddRequest(dn string, changes []AttributeChange) *ldap.AddRequest {
+	// Build the add request
 	r := NewAddRequest(dn, nil)
 
+	// Add the attributes
 	for _, change := range changes {
+		// Check if the attribute is being deleted
 		if change.Action != "add" && change.Action != "replace" {
+			// Skip the attribute
 			continue
 		}
-
+		// Add the attribute
 		r.Attribute(change.Attr, change.Value)
 	}
 
